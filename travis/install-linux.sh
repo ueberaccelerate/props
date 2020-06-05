f847f6730c828df39c93e757750ebc76da1ff230
ueberaccelerate <ueberaccelerate@gmail.com>
2020-06-05 10:20:43 +0300

    -- update travis script

--- travis/install-linux.sh
+++ travis/install-linux.sh
@@ -4,14 +4,15 @@ set -e
 # Uninstall the default version provided by Ubuntu's package manager
 sudo apt remove --purge --auto-remove cmake
 
-# Go to the official CMake webpage, then download and install
+
 version=3.16
 build=5
 mkdir ~/temp
 cd ~/temp
-wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh 
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
 
-sudo mkdir /opt/cmake
-sudo sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
-
-sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+./bootstrap
+make -j$(nproc)
+sudo make install

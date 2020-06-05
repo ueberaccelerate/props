ea55fd065dd7230f9af54a6e81a5dd52408f4bef
ueberaccelerate <ueberaccelerate@gmail.com>
2020-06-05 11:38:08 +0300

    -- update

--- travis/install-linux.sh
+++ travis/install-linux.sh
@@ -12,7 +12,7 @@ CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
 CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
 CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
 
-wget --quiet $CMAKE_URL
+wget $CMAKE_URL
 mkdir -p $CMAKE_DIR
 tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
